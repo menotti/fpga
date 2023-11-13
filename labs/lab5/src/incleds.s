b3e840a8c1fb6ba4766b186b6a0586b205edc673
Martins <thiago.martins@estudante.ufscar.br>
2023-11-13 17:00:26 -0300

    Fixing random issue when s2 register equals to 0 + hex code in asm source files

--- labs/lab5/src/incleds.s
+++ labs/lab5/src/incleds.s
@@ -1,10 +1,20 @@
 .text
     xor x1,  x1, x1
-    li  x2, 0x400000 /* MMIO */
+    li  x2, 0x400000 # MMIO 
 loop:
-    lw  x5, 0x10(x2) /* SWs */
+    lw  x5, 0x10(x2) # SWs 
     add x1,  x1, x5
-    sw  x1, 0x04(x2) /* LEDs */
-    sw  x1, 0x08(x2) /* 7SEGs */
+    sw  x1, 0x04(x2) # LEDs 
+    sw  x1, 0x08(x2) # 7SEGs 
     j loop
 
+/*
+0010c0b3
+00400137
+00010113
+01012283
+005080b3
+00112223
+00112423
+ff1ff06f
+*/
--- labs/lab5/src/sw2leds.s
+++ labs/lab5/src/sw2leds.s
@@ -1,7 +1,7 @@
 .text
-    li x2, 0x400004 /*  LEDs */
-    li x3, 0x400008 /* 7SEGs */
-    li x4, 0x400010 /*   SWs */
+    li x2, 0x400004 #  LEDs 
+    li x3, 0x400008 # 7SEGs 
+    li x4, 0x400010 #   SWs 
     li x5, 0xFF
 loop:
     lw x1, 0(x4)
@@ -12,3 +12,19 @@ segs:
 led:    
     sw x1, 0(x2)
     jal loop
+
+/*
+00400137
+00410113
+004001b7
+00818193
+00400237
+01020213
+0ff00293
+00022083
+0012d663
+0011a023
+ff5ff0ef
+00112023
+fedff0ef
+*/
